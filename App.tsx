7ea331da4333d2d6307511895dfa3edc62e94059
VlaDos <vlados@MacBook-Pro-chisto4MacPro.local>
2023-04-09 16:32:27 +0300

    added navigation stack

--- App.tsx
+++ App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {StatusBar} from 'react-native';
 import {Provider} from 'react-redux';
 import {store} from './src/store';
+import {RootNavigation} from './src/store/navigationStack/rootNavigationStack';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <Provider store={store}>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-      </SafeAreaView>
-    </Provider>
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <NavigationContainer>
+          <StatusBar />
+          <RootNavigation />
+        </NavigationContainer>
+      </Provider>
+    </SafeAreaProvider>
   );
 }
 
